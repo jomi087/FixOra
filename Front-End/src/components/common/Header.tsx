f1401b32dc0de642184656cf5bce4268d962be66
jomi087 <jomi>
2025-05-16 03:15:14 +0530

    create a header section

---
+++ Front-End/src/components/common/Header.tsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+
+interface HeaderProps {
+    className : string
+}
+
+
+const Header:React.FC<HeaderProps> = ({className}) => {
+    return (
+        <header className={`text-center px-5 pt-4 w-full md:fixed ${className}`}>
+            <Link to="/" className=" text-4xl font-extrabold tracking-tight text-blue-700 select-none"  aria-label="FixOra Home" >
+            FixOra
+            </Link>
+        </header>
+    )
+}
+
+export default Header
