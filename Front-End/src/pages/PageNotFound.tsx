7f3f91579a8b720cbb4c2725acb280b3c4700ae6
jomi087 <jomi>
2025-05-16 21:23:25 +0530

    re-write

--- Front-End/src/pages/PageNotFound.tsx
+++ Front-End/src/pages/PageNotFound.tsx
@@ -1,12 +1,12 @@
-import {BGImage_404} from '../../utils/constant'
+import {BGImage_404} from '../utils/constant'
 
-const PageNotFound = () => {
+const PageNotFound : React.FC = () => {
     return (
         <div className="flex items-center justify-center min-h-screen px-4 bg-cover bg-center"
             style={{ backgroundImage: BGImage_404 }}>
         <div className="text-center">
             <h1 className="text-4xl font-bold text-gray-800 mb-4">404 Page Not Found</h1>
-            <p className="text-lg text-gray-600 mb-6">The page you requested does not exist.</p>
+            <p className="text-lg text-gray-600 mb-6" >The page you requested does not exist.</p>
             <a
                 href="/"
                 className="inline-flex items-center px-6 py-2 bg-blue-600 text-white text-sm font-medium rounded-3xl hover:bg-blue-700 transition"
