4aa4403c7a5d1c76f69f1af9dda08e08d08c29e2
jomi087 <jomi>
2025-06-03 00:49:33 +0530

    instead of passing token to front-end, directly set to cokkie

--- Back-End/src/interfaces/controllers/AuthController.ts
+++ Back-End/src/interfaces/controllers/AuthController.ts
@@ -12,17 +12,36 @@ export class AuthController {
       console.log("enterd", req.body)
       const userData: SignupDTO = req.body;
 
-      const result = await this.signupUseCase.execute(userData);
+      const token = await this.signupUseCase.execute(userData);
+
+      res.cookie('tempToken', token, {
+        httpOnly: true,
+        secure: process.env.NODE_COOKIE_ENV === "production", //now its false //later while converting it to http to https we have to make it true , so this will not allow the cookie to be sent over http ,currently it will alowed in both http and https  
+        sameSite: "lax",
+        maxAge: 5 * 60 * 1000
+      })
 
       res.status(200).json({
           success: true,
           message: "OTP sent to your email",
-          token: result
       });
 
     } catch (error: any) {
       console.error("Signup error:", error);
       res.status(500).json({ success: false, message: "Internal server error" });
+    }
+  }
+
+  async verifyOtp(req: Request, res: Response): Promise<void>{
+    try {
+      //logic get otp from front end
+      //get email from jwt  and
+      //  find the wether there such  email and otp exist or not
+      //  if not the otp has expired  guide towards re-send otp 
+      //  else if yes and the otp are same then redirect to home page and save the data from jwt to db  
+    } catch (error: any) {
+      console.error("Signup error:", error);
+      res.status(500).json({success:false, message : 'Internal server error'})
     }
   }
 }
