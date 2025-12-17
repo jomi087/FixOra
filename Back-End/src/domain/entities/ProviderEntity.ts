fae93a7269ff9910a5486fcfd2010d990b801a57
jomi087 <jomijoseph087@gmail.com>
2025-12-17 12:04:48 +0530

    refactor: Remove unused ProviderWithDetails interface

--- Back-End/src/domain/entities/ProviderEntity.ts
+++ Back-End/src/domain/entities/ProviderEntity.ts
@@ -6,7 +6,7 @@ export interface Provider {
   dob: Date;
   gender: Gender;
   serviceId: string;
-  specializationIds: string[]; // subcategoriesa
+  specializationIds: string[];
   profileImage: string;
   serviceCharge: number;
   kyc: {
@@ -19,49 +19,4 @@ export interface Provider {
   isOnline: boolean;
   createdAt?: Date;    
   updatedAt?: Date;      
-}
-
-export interface ProviderWithDetails {
-  providerId : string
-  user: {
-    userId: string,
-    fname: string;
-    lname: string;
-    email: string;
-    mobileNo: string;
-    isBlocked: boolean;
-    location: {
-      houseinfo?: string;
-      street?: string;
-      district: string;
-      city: string;
-      locality: string;
-      state: string;
-      postalCode: string;
-      coordinates: {
-          latitude: number;
-          longitude: number
-      };
-    };
-  };
-  dob: Date;
-  gender: Gender;
-  service: {
-    categoryId: string
-    name: string;
-    subcategories: {
-        subCategoryId: string;
-        name: string
-    }[];
-  };
-  profileImage: string;
-  serviceCharge: number;
-  kyc : {
-    idCard: string;
-    certificate: {
-        education: string;
-        experience?: string;
-    };
-  },
-  isOnline: boolean;
-}
+};
