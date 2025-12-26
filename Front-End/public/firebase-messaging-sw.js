7954694596ebeb7e7e7e44ec5b5d9b94a3f34ef6
jomi087 <jomijoseph087@gmail.com>
2025-12-27 01:18:55 +0530

    feat: add Firebase messaging service and configuration for push notifications

---
+++ Front-End/public/firebase-messaging-sw.js
@@ -0,0 +1,12 @@
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+});
+
+firebase.messaging();
